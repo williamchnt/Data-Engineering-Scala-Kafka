edb257f0436f71e677d985f6c50b0e75a31205b6
williamchnt <chenut.william@gmail.com>
2021-10-29 14:35:04 +0200

    Add get lines

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,8 +4,9 @@ import scala.io.BufferedSource
 import scala.io.StdIn.readLine
 
 object main extends App {
-  val bufferedSource = io.Source.fromFile("data/airports.csv")
-  print("Query or Reports: ")
+  val airports = io.Source.fromFile("data/airports.csv","utf-8").getLines
+  print(airports)
+  print("Query or Reports (Q or R): ")
   val option = readLine()
 
   print(option)
