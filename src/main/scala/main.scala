7a2395c22c1a91cfad79343505dd1e40d08fecd5
williamchnt <chenut.william@gmail.com>
2021-11-05 15:09:44 +0100

    add def

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,8 +4,23 @@ import scala.io.BufferedSource
 import scala.io.StdIn.readLine
 
 object main extends App {
-  val airports = model.LoadAirports
-  print(airports.take(1).next())
-  print("Query or Reports (Q or R): ")
+  //val airports = model.LoadAirports
+  //println(airports.take(1).next())
+  println("Query or Reports (Q or R): ")
   val option = readLine()
+  
+  Start(option)
+  def Start(option : String) = option match {
+    case "Q" => Query()
+    case "R" => Reports()
+    case _ => println("Commande inconnue")
+   }
+
+  def Reports() =  {
+    println("Reports")
+  }
+
+  def Query() =  {
+    println("Query")
+  }
 }
