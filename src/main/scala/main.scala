b1f472c8e459e058f9ad0ae05d0638e9b82f7736
williamchnt <chenut.william@gmail.com>
2021-11-08 23:22:33 +0100

    Update main

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,6 +3,10 @@ import scala.collection.MapView
 import scala.io.BufferedSource
 import scala.io.StdIn.readLine
 import Utils.API
+import Class.Airport
+import Class.Runway
+import Class.Country
+import main.{airports, countries, runways}
 
 object main extends App {
   println("Sélectionner votre option:" +
@@ -10,23 +14,22 @@ object main extends App {
     "\n2 - Reports" +
     "\nAnything - Quitter")
   val option = scala.io.StdIn.readLine()
+  val airports = API.convertFileToAirports("data/airports.csv").toList
+  val countries = API.convertFileToCountries("data/countries.csv").toList
+  val runways = API.convertFileToRunways("data/runways.csv").toList
 
-  Start(option)
-  def Start(option : String) = option match {
-    case "1" => Query()
-    case "2" => Reports()
+  Start(option,airports,countries,runways)
+  def Start(option : String,airports: List[Airport], countries: List[Country], runways: List[Runway]) = option match {
+    case "1" => Query(airports,countries,runways)
+    case "2" => Reports(airports,countries,runways)
     case _ => println("Fin du programme")
    }
 
-  val airports = API.convertFileToAirports("resources/airports.csv")
-  val countries = API.convertFileToCountries("resources/countries.csv")
-  val runways = API.convertFileToRunways("resources/runways.csv")
-
-  def Reports() =  {
+  def Reports(airports: List[Airport], countries: List[Country], runways: List[Runway]) =  {
     println("Reports")
   }
 
-  def Query() =  {
+  def Query(airports: List[Airport], countries: List[Country], runways: List[Runway]) =  {
     println("Query")
   }
 }
